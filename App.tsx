7cb31c822e8de08f0612bec7be70267fcceaa310
imasuarezpino <isuarezp@fi.uba.ar>
2026-04-13 21:56:18 -0300

    chore: add react query provider

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { RootNavigator } from "@/navigation";
 
 export default function App() {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <RootNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <RootNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
