8d4bf85cfa6648ceca9fe50cccb52bbe3734054e
Franco Fusco <ffusco@inase.gob.ar>
2026-04-15 18:37:23 -0300

    feat: Creation of model of user

---
+++ src/entities/user/model/types.ts
@@ -0,0 +1,15 @@
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface RegisterCredentials {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  access_token: string;
+  token_type: string;
+}
