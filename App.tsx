05c3bac10c2f41fe15e6845c5d9e130c7e3bb7e7
valenpontaut <valenpontaut@gmail.com>
2026-04-26 14:25:57 -0300

    feat: adding safearea and deeplinking

--- App.tsx
+++ App.tsx
@@ -2,17 +2,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { RootNavigator } from "@/navigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { linking, RootNavigator } from "@/navigation";
 
 export default function App() {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <StatusBar style="auto" />
-        <RootNavigator />
-      </NavigationContainer>
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer linking={linking}>
+          <StatusBar style="auto" />
+          <RootNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
